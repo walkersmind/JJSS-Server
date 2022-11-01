eac4a3a58752b9801dd2493aca318ee33f44e2b5
석정도 <seogjeongdo@seogjeongdoui-MacBookPro.local>
2022-11-01 19:59:43 +0900

    병합: 브랜치 병합 작업

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,21 +1,24 @@
+import { TransformInterceptor } from './../common/interceptors/success.interceptor';
+import { UsersService } from './users.service';
 import { JwtAuthGuard } from './../auth/jwt/jwt.guard';
 import { LoginRequestDto } from './../auth/dto/login.request.dto';
 import { AuthService } from './../auth/auth.service';
 import { ReadOnlyUserDto } from './dto/user.dto';
 import {
   Body,
+  BadRequestException,
   Controller,
   Get,
   Post,
+  Query,
+  Res,
+  UnauthorizedException,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
-
-import { TransformInterceptor } from './../common/interceptors/success.interceptor';
-import { UserRequestDto } from './dto/users.request.dto';
-import { UsersService } from './users.service';
 import { CurrentUser } from 'src/common/decorators/user.decorator';
+import { UserRequestDto } from './dto/users.request.dto';
 import { User } from './users.schema';
 
 @UseInterceptors(TransformInterceptor)
@@ -28,7 +31,7 @@ export class UsersController {
 
   @ApiOperation({ summary: '피드 가져오기' })
   @UseGuards(JwtAuthGuard)
-  @Get()
+  @Get('feed')
   getFeed(@CurrentUser() user: User) {
     console.log(user);
     return user.readOnlyData;
@@ -44,7 +47,7 @@ export class UsersController {
     type: ReadOnlyUserDto,
   })
   @ApiOperation({ summary: '회원가입' })
-  @Post()
+  @Post('signup')
   async sighup(@Body() body: UserRequestDto) {
     return await this.usersService.signUp(body);
   }
@@ -53,6 +56,42 @@ export class UsersController {
   @Post('login')
   logIn(@Body() data: LoginRequestDto) {
     return this.authService.jwtLogin(data);
+  }
+
+  @Get('')
+  kakaoLoginCode(@Res() res): any {
+    res.send(`
+    <div><a
+      href="https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=9f7cabe5fceeb31eb40f58fc5d78c7e9&redirect_uri=http://localhost:3000/users/oauth/callback/kakao"
+      ><img src="https://developers.kakao.com/tool/resource/static/img/button/login/full/ko/kakao_login_medium_narrow.png"></a
+    ></div>
+    `);
+  }
+
+  @ApiOperation({ summary: '카카오 로그인 코드 받기' })
+  @Get('oauth/callback/kakao')
+  KakaoLoginToken(@Query('code') code: string, @Res() res) {
+    return res.json({ kakaoCode: code });
+  }
+
+  @ApiOperation({ summary: '카카오 로그인 토큰' })
+  @Post('oauth/callback/kakao')
+  async kakaoLogin(@Body() body: any): Promise<any> {
+    try {
+      const { code } = body;
+      if (!code) {
+        throw new BadRequestException('카카오 로그인 정보가 없습니다.');
+      }
+      const kakaoUserLoginInfo = await this.usersService.kakaoLogin(code);
+
+      console.log(kakaoUserLoginInfo);
+
+      //카카오 로그인 정보로 로그인 또는 회원가입
+      return await this.usersService.signUp(kakaoUserLoginInfo);
+    } catch (e) {
+      console.log(e);
+      throw new UnauthorizedException();
+    }
   }
 
   // logout api
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Model } from 'mongoose';
+import { Observable } from 'rxjs';
+import * as qs from 'qs';
+import { JwtService } from '@nestjs/jwt';
+import axios from 'axios';
+
+// import { HttpService, HttpModule } from '@nestjs/axios';
+
 import * as bcrypt from 'bcrypt';
 import { InjectModel } from '@nestjs/mongoose';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
@@ -11,6 +18,7 @@ import { UsersRepository } from './users.repository';
 export class UsersService {
   constructor(private readonly userRepository: UsersRepository) {}
   private saltOrRounds = 10;
+
   async signUp(body: UserRequestDto) {
     const { email, name, password } = body;
     const isUserExist = await this.userRepository.existByEamil(email);
@@ -21,13 +29,92 @@ export class UsersService {
       );
     }
 
+    // 카카오 로그인 시 비밀번호는 처리는 어떻게 할 지? => 다른 테이블로 관리
+    // const hashedPassword = await bcrypt.hash(password, 10);
+
+    console.log('body', body);
+
     const hash = await bcrypt.hash(password, this.saltOrRounds);
     const user = await this.userRepository.create({
       email,
       name,
       password: hash,
     });
-
     return user.readOnlyData;
+  }
+
+  //카카오 로그인 서비스 구현
+  async kakaoLogin(code: any): Promise<any> {
+    const apiKey = '9f7cabe5fceeb31eb40f58fc5d78c7e9';
+    const kakaoTokenUrl = 'https://kauth.kakao.com/oauth/token';
+    const kakaoUserInfoUrl = 'https://kapi.kakao.com/v2/user/me';
+    const body = {
+      grant_type: 'authorization_code',
+      client_id: apiKey,
+      redirect_uri: 'http://localhost:3000/users/oauth/callback/kakao',
+      code,
+    };
+
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded;charshet=utf-8',
+    };
+
+    try {
+      const response = await axios({
+        method: 'POST',
+        url: kakaoTokenUrl,
+        timeout: 30000,
+        headers,
+        data: qs.stringify(body),
+      });
+      if (response.status === 200) {
+        // console.log(`2. info: ${JSON.stringify(response.data)}`);
+        const headerUserInfo = {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+          Authorization: 'Bearer ' + response.data.access_token,
+        };
+        //console.log(`3. url : ${kakaoTokenUrl}`);
+        //console.log(`4. headers : ${JSON.stringify(headerUserInfo)}`);
+        const kakaoTokenData = response.data;
+
+        //console.log(kakaoTokenData);
+        const responseUserInfo = await axios({
+          method: 'GET',
+          url: kakaoUserInfoUrl,
+          timeout: 30000,
+          headers: headerUserInfo,
+        });
+        if (responseUserInfo.status === 200) {
+          // console.log(`userinfo: ${JSON.stringify(responseUserInfo.data)}`);
+          const kakaoUserInfo = responseUserInfo.data;
+
+          // console.log(kakaoUserInfo);
+
+          //회원가입에 필요한 정보 받기
+          const hash = await bcrypt.hash(kakaoTokenData['access_token'], 10);
+          const registerUserInfo = {
+            id: kakaoUserInfo['id'],
+            name: kakaoUserInfo['kakao_account']['profile']['nickname'],
+            email: kakaoUserInfo['kakao_account']['email'],
+            password: hash,
+          };
+
+          const userTokens = {
+            access_token: kakaoTokenData['access_token'],
+            refresh_token: kakaoTokenData['refresh_token'],
+          };
+
+          return registerUserInfo;
+          //이 정보로 가입이 안되어 있으면 데이터베이스에 가입시키고
+          //가입되어 있다면 로그인 코드 발급
+          //가입되어 있지만 중복 처리를 못하는 오류
+        } else {
+          // console.log('test');
+          throw new UnauthorizedException();
+        }
+      }
+    } catch (e) {
+      // console.log(e);
+    }
   }
 }
